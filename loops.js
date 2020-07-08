8561e8115baf9ff9b97543a7541f5362cc4ee155
Barsha Dahal <dahalbarsha19@gmail.com>
2020-07-08 19:25:51 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,13 +16,13 @@ function whileLoop(num){
 return 'done';
 }
 
-let i = 0;
-function incrementVariable(){
-  i=i+1;
-  return i;
-}
+
 function doWhileLoop(int){
   do{
+    let i = 0;
+    function incrementVariable(){
+      i=i+1;
+      return i;
     console.log ("I run once regardless.");
   }
   while (incrementVariable()<int);
